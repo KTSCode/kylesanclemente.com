e017f50d6a6f7bf333387e46b05a9dca8690cf4a
Kyle San Clemente <kyle@oomba.com>
2017-11-22 10:57:57 -0800

    fixed a few things

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule App.Mixfile do
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   # Configuration for the OTP application
